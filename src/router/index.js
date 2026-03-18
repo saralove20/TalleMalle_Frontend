4128e76e28dec5702866db575f9b874949d022ef
saralove20 <saralove20@naver.com>
2026-03-18 16:08:55 +0900

    [Feat] 회원가입 본인인증 추가된 페이지로 교체, 본인인증 기능 구현 완료

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Main from '@/views/service/Main.vue'
 import Login from '@/views/auth/Login.vue'
 import SocialLoginSuccess from '@/views/auth/SocialLoginSuccess.vue'
 import Signup from '@/views/auth/Signup.vue'
+import SignupNew from '@/views/auth/SignupNew.vue'
 import signupExtraInfo from '@/views/auth/SignupExtraInfo.vue'
 import Chat from '@/views/service/Chat.vue'
 import MyPage from '@/views/user/MyPage.vue'
@@ -87,6 +88,7 @@ const router = createRouter({
       meta: { hideNavbar: true },
     },
     { path: '/signup', name: 'signup', component: Signup, meta: { hideNavbar: true } },
+    { path: '/signup-new', name: 'signupNew', component: SignupNew, meta: { hideNavbar: true } },
     {
       path: '/signup/extra',
       name: 'signupExtraInfo',
