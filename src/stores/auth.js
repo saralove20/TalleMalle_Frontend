9acb8f9b966a7829c78cc1fb1f33758897f91f17
saralove20 <saralove20@naver.com>
2026-03-24 02:12:19 +0900

    [Fix] authStore 수정
    - localStorage 저장 로직 삭제했는데 다시 생겨서 삭제

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -11,40 +11,30 @@ import { defineStore } from 'pinia'
  * 2. AUTH STORE (통합 관리)
  * ==============================================================================
  */
+
+// 다른 페이지에서 const authStore = useAuthStore() 변수 선언해주고
+// authStore.user, authStore.user.id, authStore.user.email 이런식으로 사용하면 됨
+  
 export const useAuthStore = defineStore('auth', () => {
-    // 1. STATE (상태)
     const user = ref(null)         // 프로필 정보 (idx, nickname, imageUrl 등)
 
-
-    // 2. ACTIONS (기능)
-    // 로그인 처리 (이제 persist가 자동으로 저장해주므로 localStorage.setItem 불필요)
     function login(userInfo) {
-        user.value = userInfo
-    }
-
-    // 로그아웃 처리
-    function logout() {
-        user.value = null
-
-        // Pinia Persist 데이터 초기화 및 이동
-        localStorage.removeItem('auth')
-        window.location.href = '/login'
+      user.value = userInfo
     }
 
     // 유저 정보 갱신 (프로필 수정 등)
     function updateUser(newInfo) {
-        if (user.value) {
-            user.value = { ...user.value, ...newInfo }
-        } else {
-            user.value = newInfo
-        }
+      if (user.value) {
+        user.value = { ...user.value, ...newInfo }
+      } else {
+        user.value = newInfo
+      }
     }
 
-    return { 
-        user, history,
-        login, logout, updateUser
+    function logout() {
+        user.value = null
+        window.location.href = '/login'
     }
-}, {
-    // [핵심] 이 한 줄로 새로고침 시에도 모든 상태가 로컬 스토리지에 자동 저장/복구됩니다.
-    persist: true 
+
+    return { user, login, logout, updateUser }
 })
